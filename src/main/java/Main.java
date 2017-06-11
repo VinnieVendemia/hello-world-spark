deaddf9295b333e96db24027db30cb457be8b822
vvendemia <vvendemia1@gmail.com>
2017-06-11 11:41:47 -0400

    Add get with name param

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,12 @@ import static spark.Spark.*;
 public class Main {
     public static void main(String[] args) {
         get("/hello", (req, res) -> "Hello World");
+        
+        
+	     // matches "GET /hello/foo" and "GET /hello/bar"
+	     // request.params(":name") is 'foo' or 'bar'
+	     get("/hello/:name", (request, response) -> {
+	         return "Hello: " + request.params(":name");
+	     });
     }
 }
